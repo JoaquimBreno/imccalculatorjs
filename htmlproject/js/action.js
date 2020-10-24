a16435ac3aa67f9ff379c2ce982fb7a76f065c40
Joaquim Breno <winz993@gmail.com>
2020-10-23 22:27:23 -0300

    htmlproject deleted

--- htmlproject/js/action.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#items")
-.addEventListener("wheel", event => {
-    if(event.deltaY > 0){
-        event.target.scrollBy(300, 0)
-    } else {
-        event.target.scrollBy(-300, 0)
-    }
-})
--- htmlproject/js/jquery-hello.js
+++
@@ -1,5 +0,0 @@
-$("menu-2 ul li a").hover( function() {
-    $("menu-2 ul li a").transition({'font-size': '50px'}, 1000);
-    }, function(){
-    $("menu-2 ul li a").transition({"font-size":"20px"}, 1000);    
-    });
