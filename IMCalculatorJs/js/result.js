c763ca649257e59ccc19456247ecbb1e2238e279
Joaquim Breno <62159887+JoaquimBreno@users.noreply.github.com>
2021-02-20 20:36:03 -0300

    Share Js Season

--- IMCalculatorJs/js/result.js
+++ IMCalculatorJs/js/result.js
@@ -1,9 +1,20 @@
 window.onload = function(){
-    var cor = localStorage.getItem("color");
+    var x =document.getElementById("box");
     var message = localStorage.getItem("message");
     var gender = localStorage.getItem("gender");
-
-    document.getElementById("box").innerHTML=message;
-    document.getElementById("box").style.backgroundColor=cor;
+    x.innerHTML=message;
+    x.addEventListener("mouseover", colorone); 
+    x.addEventListener("mouseout", colortwo); 
+    
+    
     console.log(gender);
 }
+
+function colorone(){
+    document.getElementById("box").style.backgroundColor="#42424200";
+}
+function colortwo(){
+    var cor = localStorage.getItem("color");
+    document.getElementById("box").style.backgroundColor=cor;
+}
+
